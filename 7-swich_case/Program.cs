7563a49eb34ec7d09ffcab512b142d0f3bf367fd
hllerden <halilxerden@gmail.com>
2023-02-14 00:00:58 +0300

    switch case eklendi

---
+++ 7-swich_case/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _7_swich_case;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Case için seçim yapiniz:  ");
+        //byte a =Console.ReadLine(Convert.ToByte);
+        int month =DateTime.Now.Month;
+
+
+        switch (month)
+        {
+            case 2:
+            Console.WriteLine("Şubat ayindasiniz");
+            break;
+
+            default:
+            break;
+
+
+        }
+        //Console.WriteLine("Hello, World!");
+    }
+}
