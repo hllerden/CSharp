0f1fe44472abd2dfdcb164e5ba79eda26d09ead9
hllerden <halilxerden@gmail.com>
2023-02-14 11:28:54 +0300

    first commit

---
+++ 8-2-While Foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _8_2_While_Foreach;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        //While: 1den bailayarak konsoldan girilen sayıya kadar
+        Console.WriteLine("Lutfen bir sayi giriniz: ");
+        int n =int.Parse(Console.ReadLine());
+        int i=1;
+        int toplam=0;
+        while (i<=n)
+        {
+            toplam+=i;
+            i++;
+        }
+
+        Console.WriteLine("Ortalama :"+(toplam/n));
+
+        // 'a' dan 'z' ye kadar tüm harfleri yazdıralım.
+
+        char character = 'a';
+        while(character < 'z')
+        {
+            Console.Write(character+" ");
+            character++;
+        }
+
+        Console.WriteLine("***** Foreach ***** ");
+        string[] arabalar = {"Bmw","Ford","Toyota","Nissan","Pegeout"} ;
+        foreach(var item in arabalar)
+        {
+            Console.WriteLine(item);
+        }
+
+
+
+    }
+}
