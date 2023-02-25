1766b06cd14238be9bd9c2de188b3691ada2385a
hllerden <halilxerden@gmail.com>
2023-02-26 00:29:11 +0300

    first commit

---
+++ Project-5-Split/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Project_5_Split;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bu Projede bir string ve 1 rakam girilecek.");
+        System.Console.WriteLine("İnput : Algoritma,1 Algoritma,22 Algoritmai,90"
+                                +"Output: Algoritma Algoritma Algoritma");
+
+        System.Console.Write("İnput : ");
+        string giris = Console.ReadLine();
+        string s2 = Regex.Replace(giris, @"[^a-zA-Z d]+", String.Empty);
+        System.Console.WriteLine(s2);
+      
+    }
+}
