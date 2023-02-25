d5b5e51c889e995b3ea4b18e2750c4fb3483a000
hllerden <halilxerden@gmail.com>
2023-02-26 00:38:35 +0300

    first commit

---
+++ Project-6-Reverse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_6_Reverse;
+class Program
+{
+    static void Main(string[] args)
+    {
+         Console.WriteLine("Bu Projede bir stringi reverse ederek ekrana yaxdıracaktir.");
+        System.Console.WriteLine("İnput : Bugün çok uykum var\n"
+                                +"Output: arv mukyu koç nügub");
+
+        System.Console.Write("İnput : ");
+        string giris = Console.ReadLine();
+        System.Console.WriteLine(giris.Reverse().ToArray());
+       
+    }
+}
